12e689273188aa166f186bedc9b3f3df2ddab861
Will Olson <will.olson11@gmail.com>
2020-08-24 17:07:31 -0700

    Use create-react-app to bootstrap front-end

---
+++ website/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This will eventually be BottlenoseMail. A disposable email service.
+        </p>
+
+        <a
+          className="App-link"
+          href="https://cyberhoodie.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Built by CyberHoodie
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
