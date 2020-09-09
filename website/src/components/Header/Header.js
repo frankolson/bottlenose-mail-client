b41c7235fd0489426817d38f8a4cc8a53e480bd9
Nick Elliston <nickelliston@gmail.com>
2020-09-08 18:12:35 -0700

    Header.js & Inbox.js Updated
    Updated header.js and inbox.js with bootstrap.  Used Jumbotron for Header.

--- website/src/components/Header/Header.js
+++ website/src/components/Header/Header.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import { Row, Col, Button } from 'react-bootstrap';
+import React from "react";
+import { Row, Col, Button, Jumbotron } from "react-bootstrap";
 
 export default function Heaser() {
   return (
     <Row className="text-center mb-5">
       <Col>
-        <h1>
-          This is BottlenoseMail!
-        </h1>
-
-        <Button href="#">Create New Inbox</Button>
+        <Jumbotron>
+          <Container>
+            <h1 className="display-4" style={{ color: "black" }}>
+              Bottlenose Mail
+            </h1>
+            <p className="lead" style={{ color: "black" }}>
+              Forget about spam, advertising mailings, hacking and attacking
+              robots. Keep your real mailbox clean and secure. Bootlenoose Mail
+              provides a temporary, secure, anonymous, free, disposable email
+              address.
+            </p>
+            <br />
+          </Container>
+        </Jumbotron>
+        <Button variant="secondary" href="#">
+          Create New Inbox
+        </Button>
       </Col>
     </Row>
   );
--- website/src/views/Inbox/Inbox.js
+++ website/src/views/Inbox/Inbox.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import React from "react";
+import { Row, Col, Form } from "react-bootstrap";
 
 export default function Inbox({ match }) {
   return (
     <Row as="main">
       <Col>
-        <p className="text-center">
-          Here is inbox: {match.params.inbox}.
-        </p>
+        <Form.Group controlId="exampleForm.ControlTextarea1" sm>
+          <Form.Label style={{ color: "black" }}>Inbox</Form.Label>
+          <Form.Control as="textarea" rows="6" />
+        </Form.Group>
       </Col>
     </Row>
   );
